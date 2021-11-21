c095e91bc2db6cde1f933628113cd578187a4278
Lê Chí Luân <chiluan6601@gmail.com>
2021-11-21 18:26:47 +0700

    upload file

---
+++ LibaryManagementSystem/LibaryManagementSystem/IBooks.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibaryManagementSystem
+{
+    interface IBooks
+    {
+        public string BookID { get; set; }
+        public string BookName { get; set; }
+        public string YearPublished { get; set; }
+        public string Edition { get; set; }
+        public int Price { get; set; }
+        public string CategoryID { get; set; }
+        public string AuthorID { get; set; }
+        public string PublisherID { get; set; }
+    }
+}
